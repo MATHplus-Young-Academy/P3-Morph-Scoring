fixed computation of median

morphomatics_med/stats/ExponentialBarycenter.py
@@ -53,7 +53,7 @@ class ExponentialBarycenter(object):
 
         # Newton-type fixed point iteration
         with Parallel(n_jobs=n_jobs, prefer='threads', verbose=0) as parallel:
-            #list_log = [mfd.connect.log(x,b) for b in data]
+            
             normed_log = lambda a : mfd.connec.log(a[0],a[1])/mfd.metric.dist(a[0],a[1])
             grad = lambda a: np.sum(parallel(delayed(normed_log)((a, b))
                                              for b in data if mfd.metric.dist(a,b) > 0), axis=0)
@@ -65,6 +65,7 @@ class ExponentialBarycenter(object):
                 else:
                     g_norm = np.linalg.norm(-g)
                 print(f'|grad|={g_norm}')
+                print("I AM HERE")
                 if g_norm < 1e-12: break
                 x = mfd.connec.exp(x, -alpha*g)
 
@@ -84,3 +85,106 @@ class ExponentialBarycenter(object):
             x = ExponentialBarycenter.compute(mfd, data)
 
         return np.sum([mfd.metric.dist(x, y) ** 2 for y in data]) / len(data)
+
+    def median_grad(mfd: Manifold, data, x):
+        list_log = [mfd.connec.log(x,b) for b in data]
+        list_normedlog = [mfd.connec.norm(x,v) for v in list_log]################################################################################
+#                                                                              #
+#   This file is part of the Morphomatics library                              #
+#       see https://github.com/morphomatics/morphomatics                       #
+#                                                                              #
+#   Copyright (C) 2021 Zuse Institute Berlin                                   #
+#                                                                              #
+#   Morphomatics is distributed under the terms of the ZIB Academic License.   #
+#       see $MORPHOMATICS/LICENSE                                              #
+#                                                                              #
+################################################################################
+
+
+import numpy as np
+from joblib import Parallel, delayed
+from joblib import parallel_backend
+
+
+from ..manifold import Manifold
+#from pymanopt.solvers import SteepestDescent
+#from pymanopt import Problem
+
+
+class ExponentialBarycenter(object):
+    """
+    Exponential barycenter, see e.g.
+    Pennec and Arsigny (2012): Exponential Barycenters of the Canonical Cartan Connection and Invariant Means on Lie Groups.
+
+    The barycenter will be a bi-invariant notion of mean in the Lie group setting and the Frechét mean for Riemamnnian manifolds.
+    (For the special case of a bi-invariant metric, both notions will agree.)
+    """
+
+    @staticmethod
+    def compute(mfd: Manifold, data, x=None, max_iter=100, n_jobs=-1,alpha=1):
+        """
+        :arg mfd: data space in which mean is computed
+        :arg data: list of data points
+        :arg x: initial guess
+        :returns: mean of data, i.e. exp. barycenter thereof
+        """
+        assert mfd.connec
+
+        # initial guess
+        if x is None:
+            x = data[0].copy() #TODO: better guess -> choose most central sample
+
+        # compute intrinsic mean
+        #cost = lambda a: 0.5 / len(data) * np.sum([mfd.metric.dist(a, b) ** 2 for b in data])
+        #grad = lambda a: np.sum([mfd.connec.log(a, b) for b in data], axis=0) / len(data)
+        # hess = lambda a, b: b
+        # problem = Problem(manifold=mfd, cost=cost, grad=grad, hess=hess, verbosity=2)
+        # x = SteepestDescent(maxiter=max_iter).solve(problem, x=x)
+
+        # Newton-type fixed point iteration
+        with Parallel(n_jobs=n_jobs, prefer='threads', verbose=0) as parallel:
+            
+            for _ in range(100):
+                g = median_grad(mfd, data, x)
+                if mfd.metric:
+                    g_norm = mfd.metric.norm(x, -g)
+                else:
+                    g_norm = np.linalg.norm(-g)
+                print(f'|grad|={g_norm}')
+                if g_norm < 1e-12: break
+                x = mfd.connec.exp(x, alpha*g)
+
+        return x
+
+    @staticmethod
+    def total_variance(mfd: Manifold, data, x=None):
+        """
+        :arg mfd: data space in which mean is computed
+        :arg data: samples
+        :arg x: center
+        :returns: total variance
+        """
+        assert mfd.connec and mfd.metric
+
+        if x is None:
+            x = ExponentialBarycenter.compute(mfd, data)
+
+        return np.sum([mfd.metric.dist(x, y) ** 2 for y in data]) / len(data)
+    
+def median_grad(mfd: Manifold, data, x):
+    w = 1.0
+    list_log = np.array([mfd.connec.log(x,b) for b in data])
+    list_normedlog = np.array([mfd.connec.norm(x,v) for v in list_log])
+    
+    normed_log_mask = list_normedlog != 0
+    list_normedlog = list_normedlog[normed_log_mask]
+    list_log = list_log[normed_log_mask]
+    
+    list_normedlog = np.expand_dims(list_normedlog,axis=-1)
+    grad = np.sum(np.divide(list_log,list_normedlog), axis = 0)
+    grad_denom = np.sum(np.reciprocal(list_normedlog), axis = 0)
+
+    G = np.divide(grad, grad_denom)
+    return G
+
+