315230eccc53cd46f5639fc59254901f1369eab0
Nicolas Klenert <nicolas_klenert@web.de>
2022-03-19 11:40:09 +0100

    [Enh] Show mesh difference in median and mean

---
+++ load_difference_of_mean_median.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import inspect
+import numpy as np
+import matplotlib.pyplot as plt
+import pyvista as pv
+from sklearn.preprocessing import normalize
+from utils import runSVMClassification, plotClassificationResults
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from test import say
+from morphomatics_med.geom import Surface
+from morphomatics_med.manifold import FundamentalCoords, PointDistributionModel, util
+from morphomatics_med.stats import StatisticalShapeModel
+
+median = np.load("./mesh_median.npy")
+mean = np.load("./mesh_mean.npy")
+
+#load faces indices
+mesh = pv.read("./data/AD/test/10064.obj")
+
+# align
+from morphomatics_med.manifold.util import align
+
+median = align(median, mean)
+
+# calculate difference
+diff = np.linalg.norm(median - mean, axis = 1)
+print(diff.shape)
+
+pl = pv.Plotter()
+pl.add_mesh(pv.PolyData(median, mesh.faces), smooth_shading=True, scalars = diff, cmap = "viridis")
+pl.view_yx()
+pl.camera.roll += 180
+pl.show()
